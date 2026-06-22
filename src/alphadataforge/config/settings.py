fe85f6c0e10e1be8fe6fdf6b0e0f68a0c3885d99
kantinan <kantinan.ruj04@gmail.com>
2026-06-22 20:43:25 +0700

    feat: add Alpha Vantage configuration keys

--- src/alphadataforge/config/settings.py
+++ src/alphadataforge/config/settings.py
@@ -1,13 +1,16 @@
 import os
+from typing import Optional
 from dotenv import load_dotenv
 
-load_dotenv()
+# Use override=True so that if the user updates .env while the Jupyter kernel is running,
+# and autoreload triggers, it will actually load the new API keys instead of keeping the old ones in os.environ.
+load_dotenv(override=True)
 
 class Settings:
     """
     Settings of the package
     """
-    TIINGO_API_KEY = os.getenv("TIINGO_API_KEY")
-    ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+    TIINGO_API_KEY: Optional[str] = os.getenv("TIINGO_API_KEY")
+    ALPHAVANTAGE_API_KEY: Optional[str] = os.getenv("ALPHAVANTAGE_API_KEY")
 
 config = Settings()
